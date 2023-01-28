d60d4674a84f6163d7380a534ff24404e05a0271
Your Name <you@example.com>
2023-01-28 10:06:22 +0530

    Added Brands and What is GPT-3 sections

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -16,7 +16,7 @@ const Header = () => {
           asked of.
         </p>
         <div className="gpt3__header-content__input">
-          <input type="email" placeholder="Your Email Address" name="" id="" />
+          <input type="email" placeholder="Your Email Address" />
           <button type="button">Get Started</button>
         </div>
         <div className="gpt3__header-content__people">
